8d180ffc4035ecedae65f779d63be94eb3511761
Roshan Jadhav <roshanjadhav33@gmail.com>
2025-06-21 09:45:22 +0530

    added webClient

--- src/main/java/com/chatgpt/chatgpt_bot/config/AppConfig.java
+++ src/main/java/com/chatgpt/chatgpt_bot/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.chatgpt.chatgpt_bot.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
@@ -11,4 +13,15 @@ public class AppConfig {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
 }
+
--- src/main/java/com/chatgpt/chatgpt_bot/service/OllamaService.java
+++ src/main/java/com/chatgpt/chatgpt_bot/service/OllamaService.java
@@ -2,32 +2,72 @@ package com.chatgpt.chatgpt_bot.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
+import java.io.Serializable;
 import java.util.Map;
 
 @Service
 public class OllamaService {
 
     private final RestTemplate restTemplate ;
+    private final WebClient webClient;
+    private final ObjectMapper mapper;
     private final String OLLAMA_API_URL = "http://localhost:11434/api/generate";
 
-    public OllamaService(RestTemplate restTemplate) {
+    public OllamaService(RestTemplate restTemplate, WebClient webClient, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.webClient = webClient;
+        this.mapper = objectMapper;
     }
 
     public String generateResponse(String prompt, String model) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        //return restCall(prompt, model);
+        return webClientCall(prompt,model);
+    }
 
+    // Synchronous block call (for simplicity)
+    private String webClientCall(String prompt, String model){
+
+        Map<String, Object> requestBody = getRequestBody(prompt, model);
+
+        String response = webClient.post()
+                .uri(OLLAMA_API_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Map<String, Object> jsonMap = mapper.readValue(response, Map.class);
+            return  jsonMap.getOrDefault("response", "No Response").toString();
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to call Ollama API: " + e.getMessage(), e);
+        }
+    }
+
+    private static Map<String, Object> getRequestBody(String prompt, String model) {
         Map<String, Object> requestBody = Map.of(
                 "model", model,
                 "prompt", prompt,
-                "stream", false // Important! to get a single JSON object, not NDJSON
+                "stream", false
         );
+        return requestBody;
+    }
+
+    private String restCall(String prompt, String model) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> requestBody = getRequestBody(prompt, model);
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
 
@@ -40,7 +80,7 @@ public class OllamaService {
 
         if (response.getStatusCode().is2xxSuccessful()) {
             String body = response.getBody();
-            ObjectMapper mapper = new ObjectMapper();
+
             try {
                 Map<String, Object> jsonMap = mapper.readValue(body, Map.class);
                 return jsonMap.getOrDefault("response", "No response").toString();
